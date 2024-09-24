64c4a9f412b9e2adbe23c279c1e19bd50f1862b0
SWAYS788 <swayam@sixthsens.ai>
2024-09-24 14:27:18 +0530

    modified app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import requests
 
-
 def main():
     st.title("Techpack API")
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
     print(uploaded_file)
-    module_name = st.text_input("Enter the module name", value="H&M, Nordstrom, Chico, Walmart, Carhartt")
+    # Define a list of module names for the dropdown
+    module_names = ["H&M", "Nordstrom", "Chico", "Walmart", "Carhartt"]
+    # Use selectbox for module_name selection
+    module_name = st.selectbox("Select the module name", module_names)
 
     if st.button("Upload and Process"):
         if uploaded_file is not None:
@@ -21,7 +23,6 @@ def main():
             else:
                 st.error("Failed to process file.")
                 st.write(response.json())
-
 
 if __name__ == "__main__":
     main()
